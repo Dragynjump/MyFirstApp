584c6cac593022a29370e3078e7327c97206e883
Dragynjump <dragynjump@hotmail.com>
2023-07-24 17:21:02 +0900

    Fixed sqlalchemy package request

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Local database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userinfo.db'
 # Online database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gtezfzjjosnmsm:ffede6d2272df73f786657a7fcba2cfb171d6a37892142eafd89b8ba9c7839b6@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d4dn9nske7ujas'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gtezfzjjosnmsm:ffede6d2272df73f786657a7fcba2cfb171d6a37892142eafd89b8ba9c7839b6@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d4dn9nske7ujas'
 bcrypt = Bcrypt(app)
 db = SQLAlchemy()
 db.init_app(app)
